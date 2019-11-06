b89b23ad91f9df910925173f49984e21b25fdf12
Andy Lu <luandy64@gmail.com>
2019-11-06 14:20:13 -0500

    Bump to v0.0.4 (#5)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-lever',
-      version='0.0.3',
+      version='0.0.4',
       description='Singer.io tap for extracting data from the Lever API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
